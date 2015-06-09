f2ecc2d204abb541292708643ed7b8750b9aad78
zpbappi <zpbappi@gmail.com>
2015-06-09 17:12:26 +1000

    fixed typo. dest > dist

--- gulpfile.js
+++ gulpfile.js
@@ -12,11 +12,11 @@ var getSourceFiles = function(){
 };
 
 gulp.task("clean-debug", function(cb){
-   return del("./dest/debug/*", cb); 
+   return del("./dist/debug/*", cb); 
 });
 
 gulp.task("clean-release", function(cb){
-   return del("./dest/release/*", cb);
+   return del("./dist/release/*", cb);
 });
 
 gulp.task("clean", ["clean-debug", "clean-release"]);
@@ -26,14 +26,14 @@ gulp.task("debug", ["clean-debug"], function(){
     .pipe(sourcemaps.init())
     .pipe(concat(fileName))
     .pipe(sourcemaps.write("./", {includeContent: false, sourceRoot: "../../src/"}))
-    .pipe(gulp.dest("./dest/debug/"));  
+    .pipe(gulp.dest("./dist/debug/"));  
 });
 
 gulp.task("release", ["clean-release"], function(){
     return getSourceFiles()
     .pipe(concat(minFileName))
     .pipe(uglify())
-    .pipe(gulp.dest("./dest/release/"));
+    .pipe(gulp.dest("./dist/release/"));
 });
 
 gulp.task("all", ["debug", "release"]);
