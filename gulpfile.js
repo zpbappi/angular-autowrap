2b1ea152f5f17cecfca3c49d31ed9d40d8756df5
zpbappi <zpbappi@gmail.com>
2015-06-09 16:40:26 +1000

    added dependent npm modules for gulp tasks, added gulp tasks for generating release and debug builds

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,41 @@
-var gulp = require('gulp');
+var gulp = require("gulp"),
+    del = require("del"),
+    concat = require("gulp-concat"),
+    sourcemaps = require("gulp-sourcemaps"),
+    uglify = require("gulp-uglify");
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log("gulp ran")
+var fileName = "angular-autowrap.js",
+    minFileName = "angular-autowrap.min.js";
+    
+var getSourceFiles = function(){
+    return gulp.src("./src/**/*.js");
+};
+
+gulp.task("clean-debug", function(cb){
+   return del("./dest/debug/*", cb); 
 });
+
+gulp.task("clean-release", function(cb){
+   return del("./dest/release/*", cb);
+});
+
+gulp.task("clean", ["clean-debug", "clean-release"]);
+
+gulp.task("debug", ["clean-debug"], function(){
+    return getSourceFiles()
+    .pipe(sourcemaps.init())
+    .pipe(concat(fileName))
+    .pipe(sourcemaps.write("./", {includeContent: false, sourceRoot: "../../src/"}))
+    .pipe(gulp.dest("./dest/debug/"));  
+});
+
+gulp.task("release", ["clean-release"], function(){
+    return getSourceFiles()
+    .pipe(concat(minFileName))
+    .pipe(uglify())
+    .pipe(gulp.dest("./dest/release/"));
+});
+
+gulp.task("all", ["debug", "release"]);
+
+gulp.task('default', ["all"]);
