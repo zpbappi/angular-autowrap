30ff3024594b8932b3e1c72373be39cb61551ccd
zpbappi <zpbappi@gmail.com>
2015-07-07 17:12:46 +1000

    script support to add files to a release, added build files to github releases for major release

--- _scripts/github-releases/major.sh
+++ _scripts/github-releases/major.sh
@@ -7,3 +7,7 @@ version="$1"
 echo "[INFO] Creating GitHub release for version $version"
 
 gh_release "$GH_TOKEN" "$version" "$TRAVIS_REPO_SLUG"
+upload_url=$( get_assets_url "$GH_TOKEN" "$TRAVIS_REPO_SLUG" "$version" )
+add_files_to_release "$GH_TOKEN" "$upload_url" "./build/angular-autowrap.js" "angular-autowrap.js" "application/javascript"
+add_files_to_release "$GH_TOKEN" "$upload_url" "./build/angular-autowrap.min.js" "angular-autowrap.min.js" "application/javascript"
+add_files_to_release "$GH_TOKEN" "$upload_url" "./build/angular-autowrap.min.js.map" "angular-autowrap.min.js.map" "application/json"
